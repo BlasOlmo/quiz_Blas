5b7914f4e5d9136e4b3bb53a15f479f73c9c2bee
BlasOlmo <blasolmo@Mac.local>
2015-06-30 17:56:37 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,12 @@
+// GET/quizes/question
+exports.question = function(req,res){
+  res.render('quizes/question', {pregunta:'capital de Italia'});
+};
+//GEWT/quizes/answer
+exports.answer = function(req,res){
+  if (req.query.respuesta === 'Roma'){
+    res.render('quizes/answer',{respuesta:'Correcto'});
+  } else {
+    res.render('quizes/answer',{respuesta:'Incorrecto'});
+  }
+};
