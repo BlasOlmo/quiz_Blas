4204652cc853ca6976433e1e7bf237af49bfa562
BlasOlmo <blasolmo@Mac.local>
2015-07-01 18:52:38 +0200

    ejercicio modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,49 @@
 var models =require ('../models/models.js');
-// GET/quizes/question
-exports.question = function(req,res){
-  models.Quiz.findAll().success(function(quiz){
-    res.render('quizes/question', {pregunta:quiz[0].pregunta});
+//autoload
+
+exports.load =function(req,res,next ,quizId){
+  models.Quiz.find(quizId).then(
+    function(quiz){
+      if(quiz){
+        req.quiz =quiz;
+        next();
+      } else { next(new Error('No existe quizId=' + quizId));}
+    }
+  ).catch(function(error){ next(error);});
+}
+
+// GET/quizes/index
+exports.index = function(req,res){
+  if (req.query.search!= null){
+    var search =("%" + req.query.search + "%").replace(/\s/g,"%");
+
+    console.log(search);
+    models.Quiz.findAll({where:["pregunta like?",search]}).then(function(quizes){
+      res.render('quizes/index.ejs', {quizes:quizes});
+    }).catch(function(error){ next(error);});
+  } else {
+    models.Quiz.findAll().then(function(quizes){
+      res.render('quizes/index.ejs', {quizes:quizes});
+    }).catch(function(error){ next(error);});
+  }
+
+};
+
+// GET/quizes/show
+
+exports.show = function(req,res){
+  models.Quiz.find(req.params.quizId).then(function(quiz){
+    res.render('quizes/show', {quiz: req.quiz});
   })
 };
 //GEWT/quizes/answer
 exports.answer = function(req,res){
-  models.Quiz.findAll().success(function(quiz){
-    if (req.query.respuesta === quiz[0].respuesta){
-      res.render('quizes/answer',{respuesta:'Correcto'});
-    } else {
-      res.render('quizes/answer',{respuesta:'Incorrecto'});
+  var resultado ="Incorrecto";
+  models.Quiz.find(req.params.quizId).then(function(quiz){
+    if (req.query.respuesta === req.quiz.respuesta){
+      resultado= "Correcto"
     }
+    res.render('quizes/answer',{quiz:quiz,respuesta:resultado});
   })
 };
 exports.author = function (req,res){
